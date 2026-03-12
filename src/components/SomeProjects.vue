55de984bd3743c93c50eb57169ee984f1678191a
imyusi_ <zayyan3002@gmail.com>
2026-03-13 06:53:38 +0700

    feat(ui): implement 3-column grid for Some Projects on all screens

--- src/components/SomeProjects.vue
+++ src/components/SomeProjects.vue
@@ -31,7 +31,7 @@
       </p>
 
       <!-- Projects Grid -->
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-7">
+      <div class="grid grid-cols-3 gap-3 lg:gap-7">
         <div
           v-for="(project, index) in projects"
           :key="project.name"
