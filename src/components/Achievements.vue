bc55a9a3ffdb9676d1e9205927e8b1f5e66c9036
imyusi_ <zayyan3002@gmail.com>
2026-03-12 12:16:41 +0700

    feat : add accent 3

--- src/components/Achievements.vue
+++ src/components/Achievements.vue
@@ -83,7 +83,7 @@
     <img
         src="/images/accent_3.png"
         alt="Accent Shape"
-        class=" absolute bottom-0 left-0 w-32 lg:w-180 pointer-events-none transform rotate-180 z-0"
+        class=" absolute bottom-[-0.5rem] left-0 w-32 lg:w-180 pointer-events-none transform rotate-180 z-0"
       />
   </section>
 </template>
