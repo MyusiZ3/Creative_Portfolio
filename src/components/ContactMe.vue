c80d509dddf49f38854c1bc9de3198e1406241e9
imyusi_ <zayyan3002@gmail.com>
2026-03-26 12:19:14 +0700

    change contactme bg color to #171717

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="contact" class="bg-[#141414] py-20 px-6 md:px-10 lg:px-16 xl:px-40 relative overflow-hidden">
+  <section id="contact" class="bg-[#171717] py-20 px-6 md:px-10 lg:px-16 xl:px-40 relative overflow-hidden">
     <div class="container mx-auto relative z-10">
       <div class="flex flex-col md:flex-row items-center justify-between gap-12 md:gap-8">
         <div 
