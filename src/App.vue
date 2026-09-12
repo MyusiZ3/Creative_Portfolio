24afd6e68d6050bf29b085bb58d4c8300740a21f
imyusi_ <zayyan3002@gmail.com>
2026-09-12 22:35:01 +0700

    feat(analytics): add vercel analytics and speed insights

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { RouterView } from "vue-router";
+import { Analytics } from "@vercel/analytics/vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 </script>
 
 <template>
+  <Analytics />
+  <SpeedInsights />
   <RouterView />
 </template>
 
